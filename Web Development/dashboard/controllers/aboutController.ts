d0de38c7fb084b01b05c7aeec054b8f2d6ca9013
an-Iceberg <a.g.marosi@bluewin.ch>
2021-08-23 12:49:36 +0200

    renamed a folder

--- Web Development/dashboard/controllers/aboutController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import path from "path";
-
-let aboutPath = path.join(__dirname, '../views/about.html');
-
-export function about(request: Request, response: Response) {
-    response.sendFile(aboutPath);
-}
--- Web Development/dashboard/controllers/contactController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import path from "path";
-
-let contactPath = path.join(__dirname, '../views/contact.html');
-
-export function contact(request: Request, response: Response) {
-    response.sendFile(contactPath);
-}
