dc801f639337369562a0806c62f9e81379aa9b73
an-Iceberg <a.g.marosi@bluewin.ch>
2021-12-05 14:34:22 +0100

    added solution to problem #16

---
+++ LeetCode Solutions/16_3sum_closest.cpp
@@ -0,0 +1,107 @@
+
+#include <iostream>
+#include <vector>
+
+// Returns the positive differernce between target and sum
+int distance(int target, int sum)
+{
+    if (target > sum)
+    {
+        return target - sum;
+    }
+    else if (target < sum)
+    {
+        return sum - target;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int threeSumClosest(std::vector<int> &numbers, int &target)
+{
+    int sum;
+    int diff = 999;
+    int result;
+    int *num1;
+    int *num2;
+    int *num3;
+
+    num1 = &numbers[0];
+    num2 = &numbers[0];
+    num3 = &numbers[0];
+
+    for (int iteration = 0; iteration < (numbers.size() * numbers.size() * numbers.size()); iteration++)
+    {
+        // Only calcualting the sum if the pointers point to distinct addresses
+        if(num1 != num2 && num1 != num3 && num2 != num3)
+        {
+            sum = *num1 + *num2 + *num3;
+            
+            if(sum == target)
+            {
+                return sum;
+            }
+
+            // If the current sum is closer to the target than the previous sum, replace the previous result with the current sum
+            if(distance(target, sum) < diff)
+            {
+                diff = distance(target, sum);
+                result = sum;
+            }
+        }
+
+        // If this is the constellation of the pointer, all cases have been evaluated
+        if(num1 == &numbers[numbers.size() - 3] && num2 == &numbers[numbers.size() - 2] && num3 >= &numbers[numbers.size() - 1])
+        {
+            break;
+        }
+
+        // If the second pointer has reached the end and the first is about to finish, reset their positions and increment the third pointer
+        if(num1 == &numbers[numbers.size() - 2] && num2 >= &numbers[numbers.size() - 1])
+        {
+            num3++;
+            num2 = &numbers[0];
+            num1 = &numbers[0];
+            continue;
+        }
+
+        // If the first pointer has reached the end of the vector, reset it to the beginning of the vector and increment the second pointer
+        if (num1 >= &numbers[numbers.size() - 1])
+        {
+            num1 = &numbers[0];
+            num2++;
+            continue;
+        }
+
+        num1++;
+    }
+
+    return result;
+}
+
+void test(int testID, std::vector<int> numbers, int target, int expected)
+{
+    int result = threeSumClosest(numbers, target);
+
+    if (result != expected)
+    {
+        std::cout << testID << " failed expected:" << expected << " got:" << result << std::endl;
+    }
+    else
+    {
+        std::cout << testID << " passed" << std::endl;
+    }
+}
+
+int main()
+{
+    // Tests from leetcode.com
+    test(0, {-1, 2, 1, -4}, 1, 2);
+    test(1, {0, 0, 0}, 1, 0);
+    
+    // TODO: write my own tests
+
+    return 0;
+}
